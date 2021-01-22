2b3cb326f9728fee159960eb1e84d3f9a5406e32
RedCrabVb <cany245@mail.ru>
2021-01-22 19:31:10 +0600

    Add UI
    Реализация меню: играть, об игре, выход, настройки.
    В пункте настройки: качество графики, и не рабочая кнопка сброса сохранений,
    у игрока в меню паузы есть путь (из А в Б .jpg) заметка (сюжет =)), и кнопка возврата в меню

---
+++ Assets/UI/Pause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private float timer;
+    private bool ispuse, guipuse;
+    public GameObject pause_panel;
+    void Update()
+    {
+        Time.timeScale = timer;
+        ispuse = (Input.GetKeyDown(KeyCode.Escape) ? !ispuse : ispuse);
+
+        pause_panel.SetActive(ispuse);
+        Cursor.visible = ispuse;
+        Cursor.lockState = (ispuse ? CursorLockMode.None : CursorLockMode.Locked);
+        timer = (ispuse == true ? 0 : 1f);
+    }
+}
+
---
+++ Assets/UI/Qualty.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Qualty : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public void checkdropdown()
+    {
+        QualitySettings.SetQualityLevel(dropdown.value, true);
+    }
+}
