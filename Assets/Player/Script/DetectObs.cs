529bdbb2cec326b858d3cadf56ca42fa21931155
RedCrabVb <cany245@mail.ru>
2021-02-07 22:01:16 +0600

    Изменил карту, добавил снега, туман, изменил звуки. Возможно пофиксил один баг

--- Assets/Player/Script/DetectObs.cs
+++ Assets/Player/Script/DetectObs.cs
@@ -16,7 +16,7 @@ namespace Game.Parkour
             bool is_object_find = (ObjectTagName != "" && !Obstruction);
             if (is_object_find)
             {
-                bool does_the_object_have_the_correct_tag = col.GetComponent<CustomTag>().HasTag(ObjectTagName) && col.GetComponent<CustomTag>().IsEnabled;
+                bool does_the_object_have_the_correct_tag = col.GetComponent<CustomTag>() && col.GetComponent<CustomTag>().HasTag(ObjectTagName) && col.GetComponent<CustomTag>().IsEnabled;
                 bool is_correct_object = col != null;
                 if (is_correct_object && does_the_object_have_the_correct_tag) // checks if the object has the right tag
                 {
